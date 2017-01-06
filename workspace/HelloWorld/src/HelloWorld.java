66ba4357f8f3bd272514a735258ea75e4f1c6351
Devin Hill <Devin Hill>
2017-01-06 10:30:14 -0500

    updated HelloWorld

---
+++ workspace/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+//Basic Hello World Outprint Java Code. 
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        
+	    System.out.println(); //Line break
+	    System.out.println("This is a simple outprint program.");
+	    System.out.println(); //Line break
+	    System.out.println("Hello, World");
+    }
+
+}
